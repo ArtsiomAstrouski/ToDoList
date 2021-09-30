397bf78de3d1adcd7cc4fbb238e62a05916fa5d1
AntikBLR <86202029+AntikBLR@users.noreply.github.com>
2021-09-30 21:08:12 +0300

    Implemented the component of the App and  ToDo

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,5 +1,17 @@
-function ToDo() {
-	return <></>
+function ToDo({ todo, toggleTask, removeTask }) {
+	return (
+		<div key={todo.id} className="style1">
+			<div
+				className={todo.complete ? 'style1' : 'style2'}
+				onClick={() => toggleTask(todo.id)}
+			>
+				{todo.task}
+			</div>
+			<div className="style-delete" onClick={() => removeTask(todo.id)}>
+				X
+			</div>
+		</div>
+	)
 }
 
 export default ToDo
